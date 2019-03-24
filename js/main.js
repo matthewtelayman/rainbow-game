0ea3512e1b0d017b74a603d3346b6d3367d902cd
Alejandro Gomez <alejandrogomez@agomez-mac.local>
2019-03-24 00:08:49 -0400

    converted loops to class methods

--- js/main.js
+++ js/main.js
@@ -1,62 +1,84 @@
 //Elements
-const boardContainer = document.getElementById('board-container');
-const tableBody = document.getElementById('table-body')
-const tokenColors = [{
-        color: 'red',
-        hexValue: '#d63031'
-    },
-    {
-        color: 'blue',
-        hexValue: '#0984e3'
-    },
-    {
-        color: 'green',
-        hexValue: '#00b894'
-    },
-    {
-        color: 'yellow',
-        hexValue: '#f9ca24'
-    }
-];
+const tokenColors = ['red', 'green', 'yellow', 'blue'];
+let counter = 0;
+let globalSelectCheck = false
+
+//Utilities
+const chooseRandom = (arr) => {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
 
 //Create board
-let counter = 0
-for (let i = 0; i < 10; i++) {
-    let tr = document.createElement('tr');
-    for (let j = 0; j < 10; j++) {
-        let td = document.createElement('td');
-        td.setAttribute("style", "border:2px solid #341f97; ");
-        td.id = `tile-${counter}`;
-        tr.appendChild(td);
-        counter++;
+class Board {
+
+    tokenColors = []
+    numOfCols = 0
+    numOfRows = 0
+    counter = 0
+    tableBody = document.getElementById('table-body')
+
+    constructor(tokenColors, numOfRows, numOfCols) {
+        this.tokenColors = tokenColors
+        this.numOfRows = numOfRows
+        this.numOfCols = numOfCols
     }
-    tableBody.appendChild(tr);
+
+    createBoard() {
+        for (let i = 0; i < this.numOfRows; i++) {
+            let tr = document.createElement('tr');
+            for (let j = 0; j < this.numOfCols; j++) {
+                let td = document.createElement('td');
+                td.id = `tile-${counter}`;
+                tr.appendChild(td);
+                counter++;
+            }
+            this.tableBody.appendChild(tr);
+        }
+        const boardSize = this.numOfCols * this.numOfRows
+        this.placeTokens(this.tokenColors, boardSize)
+    }
+    placeTokens(tokenColors, boardSize) {
+        for (let i = 0; i < boardSize; i++) {
+            if (i != 44 && i != 45 && i != 54 && i != 55) {
+                let tile = document.getElementById(`tile-${i}`);
+                let token = document.createElement('div')
+                token.id = `token-${i}`
+                const randomColor = chooseRandom(tokenColors)
+                token.className = `${randomColor} token`
+                tile.append(token)
+            }
+        }
+    }
 }
 
-//Place tokens on board
-/*
-1.iterate over each tile.
-2. randomly place one of four-colored tokens
-3. exclude four middle tiles
-*/
-
-let token;
-counter = 0;
-for (let i = 0; i < 100; i++) {
-    let tile = document.getElementById(`tile-${i}`);
-    token = document.createElement('div')
-    //randomize utility here
-    token.id = `token-${counter}`
-    token.className = 'red token'
-    tile.append(token)
-    counter++;
+class Token extends Board {
+    constructor() {
+        super()
+    }
+    addTokenSelection() {
+        for (let i = 0; i < 100; i++) {
+            let token = document.getElementById(`tile-${i}`);
+            token.addEventListener('click', () => {
+                if (token.classList != 'select-token' && globalSelectCheck == false) {
+                    console.log(`Selected token with id ${i}`)
+                    token.classList = 'select-token'
+                    globalSelectCheck = true;
+                } else if (token.classList == 'select-token' && globalSelectCheck == true) {
+                    token.classList = ''
+                    globalSelectCheck = false;
+                }
+            })
+        }
+    }
+    addTokenValidation() {
+        //check if you can make a move.
+    }
 }
 
-//Add highlight action to board tiles
-for (let i = 0; i < 100; i++) {
-    token = document.getElementById(`tile-${i}`);
-    tile.addEventListener('click', () => {
-        console.log(`Selected token with id ${i}`)
-        token.classList.toggle('select-token')
-    })
-}
+const board = new Board(tokenColors, 10, 10)
+const token = new Token();
+//an init sequence must take place.
+board.createBoard()
+//add token actions
+token.addTokenSelection()
+token.addTokenSelection()
